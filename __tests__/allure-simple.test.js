3e33e3c610c51680c853e3cc19d74ab60dc6d94c
Rahul-bachchas-wawa <Rahul.Bachchas@wawa.com>
2020-09-08 16:49:53 +0530

    initiali commit
    initiali commit

---
+++ __tests__/allure-simple.test.js
@@ -0,0 +1,38 @@
+// describe("test", () => {
+//   test("Allure simple test", () => {
+//     allure.step("Simple step", () => {});
+//     allure.step("Simple parent step", () => {
+//       allure.step("Simple nested step", () => {});
+//     });
+//     externalStep("step parameter");
+//   });
+// });
+
+// const externalStep = param =>
+//   allure.step("External Step", () => {
+//     allure.step(`External step parameter: ${param}`, () => {});
+//     allure.step("Simple step inside test method", () => {});
+//   });
+
+
+
+// Stubbed example
+// describe("reverse title button clicked", () => {
+//     it("reverses title");
+// });
+
+describe("Post", () => {
+
+
+  describe("reverse title button clicked", () => {
+      it("reverses title", () => {
+     console.log("it should render")
+      });
+      it("Button clicked", () => {
+        console.log("it should render")
+         });
+   
+  });
+
+
+})
